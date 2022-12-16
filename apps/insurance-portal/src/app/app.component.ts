fe9a04c5485041c8f3273fb47e387425a8252a84
Stefan Haas <stefan.haas.privat@gmail.com>
2022-12-16 19:40:13 +0100

    Initial commit

---
+++ apps/insurance-portal/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'nx-giant-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'insurance-portal';
+}
